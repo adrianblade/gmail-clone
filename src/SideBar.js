1dc962f9770317f7af263bc11365e952e9f6c0b8
Adrián Catalán <adrian.catalan@travel-ims.com>
2021-01-15 20:32:34 +0100

    Add icon to lateral

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,11 +1,18 @@
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
 import React from 'react';
 import './SideBar.css';
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
 import SideBarOption from "./SideBarOption"
 import StarIcon from "@material-ui/icons/Star";
-
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
 
 function SideBar() {
     return (
@@ -18,9 +25,25 @@ function SideBar() {
             </Button>
             <SideBarOption Icon={InboxIcon} title="Inbox" number={54} selected={true}/>
             <SideBarOption Icon={StarIcon} title="Starred" number={54}/>
-            <SideBarOption Icon={InboxIcon} title="Inbox" number={54}/>
-            <SideBarOption Icon={InboxIcon} title="Inbox" number={54}/>
+            <SideBarOption Icon={AccessTimeIcon} title="Snoozed" number={54}/>
+            <SideBarOption Icon={LabelImportantIcon} title="Important" number={54}/>
+            <SideBarOption Icon={NearMeIcon} title="Sent" number={54}/>
+            <SideBarOption Icon={NoteIcon} title="Draft" number={54}/>
+            <SideBarOption Icon={ExpandMoreIcon} title="More" number={54}/>
 
+            <div className="sidebar__footer">
+                <div className="sidebar__footerIcons">
+                    <IconButton>
+                        <PersonIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <DuoIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <PhoneIcon/>
+                    </IconButton>
+                </div>
+            </div>
         </div>
     );
 }
